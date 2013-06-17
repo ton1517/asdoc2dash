96232a33d7846c4136ee950aa18a6e6ba7268e60
ton <tonton1517@gmail.com>
2013-06-17 11:00:18 +0900

    update version 0.1.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,11 @@
 CHANGES
 =======
 
+0.1.1 (2013-06-17)
+------------------
+
+- fixed problem : forget adding anchor tag in package.html
+
 0.1.0 (2013-06-16)
 ------------------
 
--- asdoc2dash/__init__.py
+++ asdoc2dash/__init__.py
@@ -4,6 +4,6 @@ asdoc2dash is tools that convert from ASDoc to Dash Docset.
 __author__ = "ton1517"
 __email__ = "tonton1517@gmail.com"
 __homepage__ = "https://github.com/ton1517/asdoc2dash"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __license__ = "MIT License"
 
